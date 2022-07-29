423f391a77e5a5dae167d43f01c915656d942368
Hugo MgM <85202545+HugooMaciel@users.noreply.github.com>
2022-07-29 15:43:26 -0300

    Add files via upload

--- ProjetoLetsCode/src/Aplicacao.java
+++ ProjetoLetsCode/src/Aplicacao.java
@@ -2,32 +2,51 @@ import Cliente.PessoaFisica;
 
 public class Aplicacao {
     public static void main(String[] args) throws Exception {
-        PessoaFisica pessoa1 = new PessoaFisica("nome", "cpf");
-        // PessoaFisica pessoa2 = new PessoaFisica("outro nome", "cpf");
+        PessoaFisica pessoa1 = new PessoaFisica("Victor", "987654321");
+        PessoaFisica pessoa2 = new PessoaFisica("Luiz", "123456789");
 
-        // System.out.println(pessoa1.getContaCorrente().getSaldo());
-
-        // pessoa1.getContaCorrente().deposito(500);
-        // System.out.println(pessoa1.getContaCorrente().getSaldo());
-
-        // pessoa1.getContaCorrente().saque(600);
-        // System.out.println(pessoa1.getContaCorrente().getSaldo());
-
-        // pessoa1.getContaCorrente().transferencia(pessoa2, 300);
-        // System.out.println(pessoa1.getContaCorrente().getSaldo());
-        // System.out.println(pessoa2.getContaCorrente().getSaldo());
-
-        // pessoa1.criarContaPoupanca(pessoa1.getNome(), pessoa1.getCpf());
-        // System.out.println(pessoa1.getContaPoupanca().getSaldoPoupanca());
-
+        //Criar conta investimento
         pessoa1.criarContaInvestimento(pessoa1.getNome(), pessoa1.getCpf());
+        pessoa2.criarContaInvestimento(pessoa1.getNome(), pessoa1.getCpf());
 
-        // System.out.println(pessoa1.getContaInvestimento().getSaldoInvestimento());
+        //Criar conta poupanca
+        pessoa1.criarContaPoupanca(pessoa1.getNome(),pessoa1.getCpf());
+        //
+        pessoa2.criarContaPoupanca("Luiz","123456789");
 
-        // pessoa1.getContaInvestimento().investimento(1000);
 
+        System.out.println(pessoa1.toString());
+        System.out.println(pessoa2.toString());
+        System.out.println();
+
+        System.out.printf("Saldo do cliente %s é de R$%.2f.\n",pessoa1.getNome(), pessoa1.getContaCorrente().getSaldo());
+        System.out.printf("Saldo do cliente %s é de R$%.2f.\n",pessoa2.getNome(), pessoa2.getContaCorrente().getSaldo());
+        System.out.println();
+
+        pessoa1.getContaCorrente().deposito(500, pessoa1.getTaxaInvestimento());
+        System.out.printf("Saldo do cliente %s é de R$%.2f.\n",pessoa1.getNome(), pessoa1.getContaCorrente().getSaldo());
+        System.out.printf("Saldo do cliente %s, na conta poupança, é de R$%.2f.\n",pessoa1.getNome(),pessoa1.getContaPoupanca().getSaldoPoupanca());
+        System.out.printf("Saldo do cliente %s, na conta investimento, é de R$%.2f.\n",pessoa1.getNome(),pessoa1.getContaInvestimento().getSaldo());
+
+        System.out.println();
+
+        pessoa1.getContaCorrente().saque(600, pessoa1.getTaxa());
+        pessoa1.getContaCorrente().saque(217, pessoa1.getTaxa());
+        System.out.printf("%s sacou R$%.2f e possui R$%.2f de saldo em conta",pessoa1.getNome(),217.00,pessoa1.getContaCorrente().getSaldo());
+        System.out.println();
+
+        pessoa1.getContaCorrente().transferencia(pessoa2, 150, pessoa1.getTaxa());
+        System.out.printf("Saldo do cliente %s é de R$%.2f.\n",pessoa1.getNome(), pessoa1.getContaCorrente().getSaldo());
+        System.out.printf("Saldo do cliente %s é de R$%.2f.\n",pessoa2.getNome(), pessoa2.getContaCorrente().getSaldo());
+        System.out.println();
+
+
+
+        //deposito na conta investimento e seu rendimento
         pessoa1.getContaInvestimento().deposito(1000, pessoa1.getTaxaInvestimento());
-        System.out.println("Saldo: " + pessoa1.getContaInvestimento().getSaldo());
+        System.out.printf("Saldo do cliente %s, na conta investimento, é de R$%.2f.\n",pessoa1.getNome(),pessoa1.getContaInvestimento().getSaldo());
+
+
 
     }
 }
